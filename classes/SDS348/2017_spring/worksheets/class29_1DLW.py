fa902d0e3cdbf7ff1c584a53dba4837c4789f234
Claus Wilke <wilke@austin.utexas.edu>
2017-04-30 16:39:31 -0500

    class 29

---
+++ classes/SDS348/2017_spring/worksheets/class29_1DLW.py
@@ -0,0 +1,9 @@
+# Probem 2
+cmd.fetch("1DLW")
+cmd.hide("all")
+cmd.show("cartoon", "all")
+cmd.select("chain_A", "chain A")
+cmd.color("brown", "(chain_A)")
+cmd.select("heme", "resn HEM")
+cmd.show("sticks", "(heme)")
+cmd.color("atomic", "(heme)")
